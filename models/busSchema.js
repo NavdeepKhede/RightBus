e8ed07a27aa89c8f61336515ec370cc2a500a02c
Himanshu Kumar <lcs2020047@iiitl.ac.in>
2023-11-29 01:21:55 +0530

    Revert "added src, destination value in availability api"
    This reverts commit 1b09901ac0584b215167087115698ee383593246.

--- models/busSchema.js
+++ models/busSchema.js
@@ -122,7 +122,7 @@ class Bus {
       currentDate = currentDate.toLocaleDateString();
       const query = `
       SELECT b.*,
-      b.totalSeats - COALESCE(COUNT(sr.seat_number), 0) AS available_seats, r.src, r.destination
+      b.totalSeats - COALESCE(COUNT(sr.seat_number), 0) AS available_seats
       FROM buses b
       JOIN routes r ON b.route_id = r.id
       LEFT JOIN seat_reservations sr ON b.id = sr.bus_id AND sr.journey_date = $4
